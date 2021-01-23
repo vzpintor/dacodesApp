de099205e48e6574daeed78a1dd4fe7543caf670
Orlando Vazquez Pintor <vzpintor@gmail.com>
2021-01-22 20:51:06 -0600

    Se recupera detalle de pelicula

--- src/app/screens/detail/movie-detail.tsx
+++ src/app/screens/detail/movie-detail.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Image, Text, View} from 'react-native';
-import {Container} from '../../shared/components/screen/screen.component';
+import {Container} from '@shared/components/screen/screen.component';
 import {detailStyles} from '@screens/detail/detail-movie-styles';
+import {connect} from 'react-redux';
+import {getMovieDetail} from '@shared/services/movies-service';
+import {IGenre} from '@shared/interfaces/movie-interface';
 
-const MovieDetail = () => {
+const MovieDetail = ({currentMovie, getMovieDetail}: any) => {
+  useEffect(() => {
+    getMovieDetail(currentMovie.id);
+  }, []);
+
   return (
     <Container preset={'scroll'} unsafe={true}>
       <View>
         <Image
           resizeMode={'cover'}
           source={{
-            uri:
-              'https://image.tmdb.org/t/p/w500/3ombg55JQiIpoPnXYb2oYdr6DtP.jpg',
+            uri: `https://image.tmdb.org/t/p/w500/${currentMovie.image}`,
           }}
           style={detailStyles.image}
         />
@@ -19,46 +25,50 @@ const MovieDetail = () => {
 
       <View>
         <View>
-          <Text style={detailStyles.title}>{'Guasón'}</Text>
+          <Text style={detailStyles.title}>{currentMovie.title}</Text>
         </View>
 
         <View style={detailStyles.item}>
           <Text style={detailStyles.titleText}>Duración:</Text>
-          <Text style={detailStyles.text}>122 min</Text>
+          <Text style={detailStyles.text}>{currentMovie.time} min</Text>
         </View>
 
         <View style={detailStyles.item}>
           <Text style={detailStyles.titleText}>Fecha de estreno:</Text>
-          <Text style={detailStyles.text}>2 Octubre 2019</Text>
+          <Text style={detailStyles.text}>{currentMovie.date}</Text>
         </View>
 
         <View style={detailStyles.item}>
           <Text style={detailStyles.titleText}>Calificación</Text>
-          <Text style={detailStyles.text}>8.5</Text>
+          <Text style={detailStyles.text}>{currentMovie.voteAverage}</Text>
         </View>
 
         <View style={detailStyles.item}>
           <Text style={detailStyles.titleText}>Géneros:</Text>
-          <Text style={detailStyles.text}>Crimen, Suspense, Drama</Text>
+          <Text style={detailStyles.text}>
+            {currentMovie.genres?.map((gender: IGenre) => gender.name + ', ')}
+          </Text>
         </View>
 
         <View style={detailStyles.item}>
           <Text style={detailStyles.titleText}>Descripción:</Text>
-          <Text style={detailStyles.text}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </Text>
+          <Text style={detailStyles.text}>{currentMovie.description}</Text>
         </View>
       </View>
     </Container>
   );
 };
 
-export default MovieDetail;
+// Map State To Props (Redux Store Passes State To Component)
+const mapStateToProps = (state: any) => {
+  return {
+    currentMovie: state.moviesReducer.currentMovie,
+  };
+};
+
+// Map Dispatch To Props (Dispatch Actions To Reducers. Reducers Then Modify The Data And Assign It To Your Props)
+const mapDispatchToProps = (dispatch: any) => ({
+  getMovieDetail: (idMovie: number) => dispatch(getMovieDetail(idMovie)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieDetail);
--- src/app/shared/services/movies-service.ts
+++ src/app/shared/services/movies-service.ts
@@ -1,8 +1,15 @@
 import {Dispatch} from 'redux';
-import {fetchMovies, setLoading} from '@stores/movies/home-action';
+import {
+  fetchMovies,
+  setCurrentMovie,
+  setLoading,
+} from '@stores/movies/home-action';
+
+const API = 'https://api.themoviedb.org/3/movie/';
+const API_KEY = '634b49e294bd1ff87914e7b9d014daed';
 
 export const getMovies = (page: number) => {
-  const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=634b49e294bd1ff87914e7b9d014daed&language=en-US&page=${page}`;
+  const URL = `${API}now_playing?api_key=${API_KEY}&language=en-US&page=${page}`;
   console.log('Loading movies in page: ', page);
   return function (dispatch: Dispatch<any>) {
     dispatch(setLoading(true));
@@ -30,29 +37,24 @@ export const getMovies = (page: number) => {
   };
 };
 
-export const getMovieDetail = (page: number) => {
-  const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=634b49e294bd1ff87914e7b9d014daed&language=en-US&page=${page}`;
-  console.log('Loading movies in page: ', page);
+export const getMovieDetail = (idMovie: number) => {
+  const URL = `${API}${idMovie}?api_key=${API_KEY}&language=en-US`;
   return function (dispatch: Dispatch<any>) {
     dispatch(setLoading(true));
     fetch(URL).then((response) => {
       response.json().then((data) => {
         const result = {
-          page: data.page,
-          totalPage: data.total_pages,
-          total: data.total_results,
-          movies: data.results.map((m: any) => {
-            return {
-              id: m.id,
-              title: m.title,
-              date: m.release_date,
-              voteAverage: m.vote_average,
-              image: m.backdrop_path,
-            };
-          }),
+          id: data.id,
+          title: data.title,
+          date: data.release_date,
+          voteAverage: data.vote_average,
+          image: data.backdrop_path,
+          description: data.overview,
+          time: data.runtime,
+          genres: [...data.genres],
         };
 
-        dispatch(fetchMovies(result));
+        dispatch(setCurrentMovie(result));
         dispatch(setLoading(false));
       });
     });
